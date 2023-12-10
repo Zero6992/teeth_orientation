3d7c15df28074ac94e41e48c38927fe3a439d359
Irokanade <michaeljs.leong@gmail.com>
2023-12-10 18:37:51 +0800

    feat: added teeth object with mask and mask area in pixels

--- maskProccesing.py
+++ maskProccesing.py
@@ -6,46 +6,24 @@ from PIL import Image
 class TeethObject:
     def __init__(self, mask):
         self.mask = mask
-        self.center, self.area = self.calculate_center_and_area(mask)
+        self.area = self.calculate_area()
 
-    @staticmethod
-    def calculate_center_and_area(mask):
-        # Find contours in the mask
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        print(self.area)
 
-        if contours:
-            # Calculate the moments of the largest contour
-            largest_contour = max(contours, key=cv2.contourArea)
-            moments = cv2.moments(largest_contour)
-
-            # Calculate the centroid (center) of the largest contour
-            if moments["m00"] != 0:
-                cX = int(moments["m10"] / moments["m00"])
-                cY = int(moments["m01"] / moments["m00"])
-                center = (cX, cY)
-            else:
-                center = None
-
-            # Calculate the area of the largest contour
-            area = cv2.contourArea(largest_contour)
-        else:
-            center = None
-            area = 0
-
-        return center, area
+    def calculate_area(self):
+        return cv2.countNonZero(self.mask)
 
 
-def change_color(img, target_color):
-    # Convert the image to NumPy array
+def get_color_mask(img, target_color):
     img_array = np.array(img)
 
     # Create a mask for non-colored pixels
     mask = np.all(img_array[:, :, :3] != target_color, axis=-1)
 
-    # Display the mask as a binary image
-    plt.subplot(1, 3, 1)
-    plt.imshow(mask, cmap='gray')
-    plt.title('Mask')
+    # # Display the mask as a binary image
+    # plt.subplot(1, 3, 1)
+    # plt.imshow(mask, cmap='gray')
+    # plt.title('Mask')
 
     # Set non-colored pixels to black
     img_array[mask, :3] = [0, 0, 0]
@@ -53,38 +31,47 @@ def change_color(img, target_color):
     # Create a new image from the NumPy array
     new_img = Image.fromarray(img_array)
 
-    # Display the new image
-    plt.subplot(1, 3, 2)
-    plt.imshow(new_img)
-    plt.title('Modified Image')
+    # # Display the new image
+    # plt.subplot(1, 3, 2)
+    # plt.imshow(new_img)
+    # plt.title('Modified Image')
 
-    # Display the target color
-    plt.subplot(1, 3, 3)
-    plt.imshow([[target_color]])
-    plt.title('Target Color')
+    # # Display the target color
+    # plt.subplot(1, 3, 3)
+    # plt.imshow([[target_color]])
+    # plt.title('Target Color')
 
-    plt.show()
+    # plt.show()
 
-    color = get_unique_colors(np.array(new_img))
-    print(color)
+    # color = get_unique_colors(np.array(new_img))
+    # print(color)
 
-    return new_img
+    return mask, new_img
 
+
+def binarizeImage(image):
+    # Convert PIL Image to NumPy array
+    if isinstance(image, Image.Image):
+        img_array = np.array(image)
+    elif isinstance(image, np.ndarray):
+        img_array = image
+    else:
+        raise ValueError("Unsupported input type. Please provide a PIL Image or a NumPy array.")
+
+    # Convert the image to grayscale
+    gray_image = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+
+    # Use cv2.threshold to create a binary image
+    ret, binary_image = cv2.threshold(gray_image, 1, 255, cv2.THRESH_BINARY)
+
+    return binary_image
 
 def get_unique_colors(image):
     # Convert the image to a NumPy array
     img_array = np.array(image)
-
     unique_colors = np.unique(img_array.reshape(-1, img_array.shape[2]), axis=0)
 
     return unique_colors
-
-def get_unique_colors_old(image_path):
-    image = cv2.imread(image_path)
-    pixels = image.reshape((-1, 3))
-    unique_colors_set = set(map(tuple, pixels))
-    unique_colors_list = list(unique_colors_set)
-    return unique_colors_list
 
 #Get bounding boxes from mask of one colour
 def get_bounding_box(mask, color):
@@ -103,9 +90,10 @@ def get_bounding_box(mask, color):
     return bbox
 
 
-# Example usage
 image_path = 'result/1/mask/mask_IMG_2898.png'
 image = Image.open(image_path)
+
+teethList = []
 
 colors = get_unique_colors(image)
 # won't include black as black is backround
@@ -117,4 +105,12 @@ for color in colors:
 
     # Check if the color is not black
     if not np.array_equal(color, black_color):
-        change_color(image, color)
+        # theres some bug with mask so use new_img for now
+        _, new_img = get_color_mask(image, color)
+
+        # convert new_img to binary image
+        binary_image = binarizeImage(new_img)
+        # plt.imshow(binary_image, cmap='gray')
+        # plt.show()
+
+        teethList.append(TeethObject(binary_image))
